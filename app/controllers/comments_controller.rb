c48714afe48d59d2f8abc0aabfe4e27394dc0dec
satendra <satendra@satendra.nppgl.com>
2017-08-04 15:14:44 +0530

    changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -28,7 +28,8 @@ class CommentsController < ApplicationController
 
 	def find_commentable
       @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
-      @commentable = Blog.find_by_id(params[:blog_id]) if params[:blog_id]
+      binding.pry
+      @commentable = Blog.find(params[:blog_id]) if params[:blog_id]
     end
 end
 
